4de8cb64edd7787efc5809922a764c6ef9816102
Nilesh <nileshpandey3@gmail.com>
2021-06-27 19:44:08 -0700

    working till end except price verification

--- features/orbitz.feature
+++ features/orbitz.feature
@@ -10,3 +10,6 @@ Feature: Orbitz flight booking system
     And Enter destination location
     And  I Pick a date range and search for flights
     Then I validate the search results
+    When I select non-stop flights filter
+    And I choose the most expensive flight and book it
+    Then Confirm my booking details
--- features/steps/orbitz_steps.py
+++ features/steps/orbitz_steps.py
@@ -1,35 +1,14 @@
 import datetime
-import time
 
 from behave import given, when, then
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 
 from features.steps.orbitz_data import home_url, home_page_title
 
-driver = webdriver.Chrome('/Users/nileshpandey/PycharmProjects/friendbuy_test/chromedriver')
-
-
-# @when("I click on start sharing")
-# def start_sharing(context):
-#     start_sharing_btn = WebDriverWait(driver, 20).until(
-#         EC.element_to_be_clickable((By.XPATH, sharing_btn)))
-#     start_sharing_btn.click()
-
-# - Visit www.orbitz.com website.
-# - Select Flights
-# - Select “Roundtrip”.
-# - Enter “Leaving from” : San Francisco and “Going to”: New York.
-# - Select “Departing” date to be 2 weeks from today and “Returning” date to be 3 weeks from today.
-# - Click on Search.
-# - Assert that the search results are rendered correctly (Ex: Departure/Arrival location and dates match the input data).
-# - Select “Nonstop” flights.
-# - Select the most expensive flight from the list.
-# - Click on “Select” and then click on “Select this fare” to book.
-# - Assert the flight details & price on the flight review page.
+driver = webdriver.Chrome('/Users/nileshpandey/Documents/chromedriver')
 
 
 @given("I am on orbitz homepage")
@@ -77,21 +56,90 @@ def pick_date(context):
     context.three_weeks = today_date + datetime.timedelta(weeks=3)
     WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, '//*[@data-stid="open-date-picker"]'))).click()
-    driver.find_element_by_xpath(f"//h2[normalize-space()='June 2021']").is_displayed()
+    driver.find_element_by_xpath(f"//h2[normalize-space()='July 2021']").is_displayed()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-        (By.XPATH, f"//button[@aria-label='Jun {context.two_weeks.day}, 2021 selected, current check in date.']"))).click()
+        (By.XPATH,
+         f"//button[@aria-label='Jul {context.two_weeks.day}, 2021 selected, current check in date.']"))).click()
     WebDriverWait(driver, 20).until(EC.element_to_be_clickable(
-        (By.XPATH, f"//button[@aria-label='Jun {context.three_weeks.day}, 2021']"))).click()
-    driver.find_element_by_xpath("//body/div[@id='app']/div[@id='app-layer-manager']/div[@id='app-layer-base']/div[contains(@class,'uitk-grid')]/div[@class='uitk-cell Storefront-Homepage']/div[@role='main']/div[@class='StorefrontWizardRegionCOMET comet-homepage-module']/div[@class='cometRegion']/figure[@class='uitk-image cometBackgroundImageFigure']/div[@class='uitk-layout-grid uitk-layout-grid-columns-small-2 uitk-layout-grid-columns-large-12 wizardCard all-t-padding-six all-x-padding-six SimpleContainer']/div[@class='uitk-card-aloha uitk-card-aloha-roundcorner-all uitk-layout-grid-item all-cell-1-1 all-b-padding-six uitk-layout-grid-item-columnspan-small-2 uitk-layout-grid-item-columnspan-large-12']/div[@class='uitk-tabs-container']/div[@class='uitk-tabs-content']/div[@id='wizard-flight-pwa']/div[@class='wizardOverHeroImage all-x-padding-six']/form[@id='wizard-flight-pwa-1']/div[@class='uitk-tabs-container']/div[@class='uitk-tabs-content']/div[@id='wizard-flight-tab-roundtrip']/div[@class='uitk-layout-grid uitk-layout-grid-gap-three uitk-layout-grid-columns-small-4 uitk-layout-grid-columns-medium-6 uitk-layout-grid-columns-large-12 uitk-spacing uitk-spacing-padding-block-three']/div[@class='uitk-layout-grid-item uitk-layout-grid-item-columnspan-small-4 uitk-layout-grid-item-columnspan-medium-6 uitk-layout-grid-item-columnspan-large-4']/div[@class='Dates']/div[contains(@class,'uitk-flex uitk-flex-row uitk-flex-gap-three uitk-flex-item uitk-date-fields uitk-flex-grow-1')]/div[@class='uitk-flex-item uitk-flex-basis-zero uitk-flex-grow-1 uitk-date-field-wrapper']/div[@class='uitk-date-picker-menu uitk-menu uitk-menu-mounted']/div[@class='uitk-date-picker-menu-container uitk-date-picker-menu-container-double uitk-menu-container uitk-menu-open uitk-menu-pos-left uitk-menu-container-autoposition uitk-menu-container-text-nowrap']/div[@class='uitk-date-picker date-picker-menu']/div[@class='uitk-flex uitk-date-picker-menu-footer']/button[@type='button']/span[1]").click()
+        (By.XPATH, f"//button[@aria-label='Jul {context.three_weeks.day}, 2021']"))).click()
+    driver.find_element_by_xpath("//body/div[@id='app']/div[@id='app-layer-manager']/div[@id='app-layer-base']/div["
+                                 "contains(@class,'uitk-grid')]/div[@class='uitk-cell Storefront-Homepage']/div["
+                                 "@role='main']/div[@class='StorefrontWizardRegionCOMET comet-homepage-module']/div["
+                                 "@class='cometRegion']/figure[@class='uitk-image cometBackgroundImageFigure']/div["
+                                 "@class='uitk-layout-grid uitk-layout-grid-columns-small-2 "
+                                 "uitk-layout-grid-columns-large-12 wizardCard all-t-padding-six all-x-padding-six "
+                                 "SimpleContainer']/div[@class='uitk-card-aloha uitk-card-aloha-roundcorner-all "
+                                 "uitk-layout-grid-item all-cell-1-1 all-b-padding-six "
+                                 "uitk-layout-grid-item-columnspan-small-2 "
+                                 "uitk-layout-grid-item-columnspan-large-12']/div[@class='uitk-tabs-container']/div["
+                                 "@class='uitk-tabs-content']/div[@id='wizard-flight-pwa']/div["
+                                 "@class='wizardOverHeroImage all-x-padding-six']/form["
+                                 "@id='wizard-flight-pwa-1']/div[@class='uitk-tabs-container']/div["
+                                 "@class='uitk-tabs-content']/div[@id='wizard-flight-tab-roundtrip']/div["
+                                 "@class='uitk-layout-grid uitk-layout-grid-gap-three "
+                                 "uitk-layout-grid-columns-small-4 uitk-layout-grid-columns-medium-6 "
+                                 "uitk-layout-grid-columns-large-12 uitk-spacing "
+                                 "uitk-spacing-padding-block-three']/div[@class='uitk-layout-grid-item "
+                                 "uitk-layout-grid-item-columnspan-small-4 uitk-layout-grid-item-columnspan-medium-6 "
+                                 "uitk-layout-grid-item-columnspan-large-4']/div[@class='Dates']/div[contains(@class,"
+                                 "'uitk-flex uitk-flex-row uitk-flex-gap-three uitk-flex-item uitk-date-fields "
+                                 "uitk-flex-grow-1')]/div[@class='uitk-flex-item uitk-flex-basis-zero "
+                                 "uitk-flex-grow-1 uitk-date-field-wrapper']/div[@class='uitk-date-picker-menu "
+                                 "uitk-menu uitk-menu-mounted']/div[@class='uitk-date-picker-menu-container "
+                                 "uitk-date-picker-menu-container-double uitk-menu-container uitk-menu-open "
+                                 "uitk-menu-pos-left uitk-menu-container-autoposition "
+                                 "uitk-menu-container-text-nowrap']/div[@class='uitk-date-picker "
+                                 "date-picker-menu']/div[@class='uitk-flex uitk-date-picker-menu-footer']/button["
+                                 "@type='button']/span[1]").click()
     WebDriverWait(driver, 20).until(
         EC.element_to_be_clickable((By.XPATH, '//button[normalize-space()="Search"]'))).click()
 
 
 @then("I validate the search results")
 def validate_results(context):
-    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//*[@value="San Francisco, CA, United States of America (SFO)"]')))
-    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, '//*[@value="London, England, UK (LON)"]')))
-    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, f'//*[@value="6/{context.two_weeks.day}/2021"]')))
-    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, f'//*[@value="6/{context.three_weeks.day}/2021"]')))
+    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space("
+                                                                                       ")='San Francisco, CA (SFO-San"
+                                                                                       " Francisco Intl.)']")))
+    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space("
+                                                                                       ")='London, England, "
+                                                                                       "UK (LON-All Airports)']")))
+    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space("
+                                                                                       ")='Jul 11']")))
+    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, "//button[normalize-space("
+                                                                                       ")='Jul 18']")))
+
+
+@when("I select non-stop flights filter")
+def non_stop_flight_filter(context):
+    driver.find_element_by_xpath('//*[@id="stops-0"]').click()
+
+
+@when("I choose the most expensive flight and book it")
+def sort_most_expensive(context):
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="listings-sort"]'))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@id="listings-sort"]/option[2]'))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[@class='uitk-card-link']"))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Continue']"))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, '//*[@data-test-id="offer-listing"][1]'))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//button[normalize-space()='Continue']"))).click()
+    WebDriverWait(driver, 20).until(
+        EC.element_to_be_clickable((By.XPATH, "//a[normalize-space()='No Thanks']"))).click()
+
+
+@then("Confirm my booking details")
+def confirm_booking_details(context):
+    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, "//h2[normalize-space()='San "
+                                                                                       "Francisco to London']")))
+    assert WebDriverWait(driver, 60).until(EC.visibility_of_element_located((By.XPATH, "//h2[normalize-space("
+                                                                                       ")='London to San "
+                                                                                       "Francisco']")))
+    driver.close()
+    driver.quit()
 
 
